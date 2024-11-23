5f874108da32d05dfc6a330ebe43fdc68ef05ced
MoneyEt <ethanngo3539@gmail.com>
2024-11-22 16:57:20 -0800

    updated recurring and task classes

--- Tasks/RecurringTask.py
+++ Tasks/RecurringTask.py
@@ -1,4 +1,25 @@
 import Task
 
 class RecurringTask(Task):
-    pass
+    def __init__(self,  name: str, taskType: str, date: str, startTime: str, duration: int, endDate: str, frequency: int):
+        super().__init__(name, taskType, date, startTime, duration)
+        self._endDate = endDate
+        self._frequency = frequency
+
+    # Getter and Setter for endDate
+    @property
+    def endDate(self):
+        return self._endDate
+
+    @endDate.setter
+    def name(self, value: str):
+        self._endDate = value
+    
+    # Getter and Setter for frequency
+    @property
+    def endDate(self):
+        return self._frequency
+
+    @frequency.setter
+    def name(self, value: str):
+        self._frequency = value
